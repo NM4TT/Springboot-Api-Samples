79686ac8b57d66e8ad629148a0793058e71db4fa
NM4TT <manuelnmatute@gmail.com>
2023-10-28 20:56:30 -0500

    ADD Basic config & folders

---
+++ soap-api-sample/src/main/java/com/nmatute/soapapisample/SoapApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.nmatute.soapapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApiSampleApplication.class, args);
+	}
+
+}
---
+++ soap-api-sample/src/test/java/com/nmatute/soapapisample/SoapApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmatute.soapapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
