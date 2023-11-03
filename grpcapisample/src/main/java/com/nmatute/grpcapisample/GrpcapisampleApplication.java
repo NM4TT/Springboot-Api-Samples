c7b02341c568ead2c86fd7bd8e4d511f0142fe4d
NM4TT <manuelnmatute@gmail.com>
2023-11-03 13:23:38 -0500

    gRPC sample init

---
+++ grpcapisample/src/main/java/com/nmatute/grpcapisample/GrpcapisampleApplication.java
@@ -0,0 +1,13 @@
+package com.nmatute.grpcapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcapisampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcapisampleApplication.class, args);
+	}
+
+}
---
+++ grpcapisample/src/test/java/com/nmatute/grpcapisample/GrpcapisampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmatute.grpcapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcapisampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
