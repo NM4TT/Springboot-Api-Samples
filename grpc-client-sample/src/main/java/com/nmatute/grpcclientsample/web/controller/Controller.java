4990eaddb9099dadd73e79936985d75db07378bd
NM4TT <manuelnmatute@gmail.com>
2023-12-03 15:41:39 -0500

    Started development in server

--- grpc-client-sample/src/main/java/com/nmatute/grpcclientsample/web/controller/Controller.java
+++ grpc-client-sample/src/main/java/com/nmatute/grpcclientsample/web/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.nmatute.grpcclientsample;
+package com.nmatute.grpcclientsample.web.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
